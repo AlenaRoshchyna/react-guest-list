d4d9d07bd79578f0127924676c4151570dc74847
Alena Roshchyna <girls.code.better@gmail.com>
2023-05-12 17:00:36 +0200

    Submit input user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,78 @@
 // import styles from './App.module.scss';
+import { useEffect, useState } from 'react';
+
+const guests = [
+  {
+    name: {
+      first: 'Nastasija',
+      last: 'Danilova',
+    },
+  },
+  {
+    name: {
+      first: 'Andrej',
+      last: 'Bogomolov',
+    },
+  },
+];
 
 export default function App() {
-  return <></>;
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [users, setUsers] = useState(guests);
+
+  return (
+    <>
+      {users.map((user) => {
+        console.log(user);
+        return (
+          <div data-test-id="guest" key={user.id}>
+            {user.name.first}
+            {user.name.last}
+          </div>
+        );
+      })}
+
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <label>
+          First name
+          <input
+            value={firstName}
+            onChange={(event) => {
+              setFirstName(event.currentTarget.value);
+            }}
+          />
+        </label>
+        <label>
+          Last name
+          <input
+            value={lastName}
+            onChange={(event) => {
+              setLastName(event.currentTarget.value);
+            }}
+          />
+        </label>
+        <button
+          onClick={() => {
+            const newUser = {
+              name: {
+                first: firstName,
+                last: lastName,
+              },
+            };
+
+            const newGuest = [...users, newUser];
+
+            setUsers(newGuest);
+          }}
+        >
+          Add guest
+        </button>
+      </form>
+    </>
+  );
 }
